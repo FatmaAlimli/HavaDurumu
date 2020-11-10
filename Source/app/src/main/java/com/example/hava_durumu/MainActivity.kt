46da16bd39be6404a8a60ee625fd5a27e8832356
fatma <fatma.alimli35@gmail.com>
2020-11-10 21:23:52 +0300

    [main] - Project creation

---
+++ Source/app/src/main/java/com/example/hava_durumu/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hava_durumu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
